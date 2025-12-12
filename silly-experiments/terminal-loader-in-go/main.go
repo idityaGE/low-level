dd8f1262b10c6d57efbf1de98ab15fc1e815cfee
idityaGE <am44910606@gmail.com>
2025-12-13 03:03:51 +0530

    clean baby

--- silly-experiments/terminal-loader-in-go/main.go
+++ silly-experiments/terminal-loader-in-go/main.go
@@ -1,2 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func main() {
+	loader(50)
+}
+
+func loader(col int) {
+
+	for i := 0; i <= col; i++ {
+		random := rand.Intn(200)
+		if i == col {
+			fmt.Printf("\r[%s]  %dms", strings.Repeat("=", col+1), random)
+		} else {
+			fmt.Printf("\r[%s>%s] %dms", strings.Repeat("=", i), strings.Repeat(" ", col-i), random)
+		}
+		time.Sleep(time.Duration(random) * time.Millisecond)
+	}
+	fmt.Println()
+}
